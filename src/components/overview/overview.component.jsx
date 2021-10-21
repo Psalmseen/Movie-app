1dec9fd472d839b263dc4119e6e2676b634d3f9b
Psalmseen <55616929+Psalmseen@users.noreply.github.com>
2021-10-21 11:15:34 +0100

    Updated the collection utils and overview components to get data from feetch

--- src/components/overview/overview.component.jsx
+++ src/components/overview/overview.component.jsx
@@ -1,11 +1,30 @@
-import React from "react";import results from "../../result";
+import React, { useEffect, useState } from "react";
+// import results from "../../result";
 import Card from "../card/card.component";
-import getCollections from "../../utils/collection.utils";
-
+import  getCollections from "../../utils/collection.utils";
 
 const Overview = () => {
+  const [results, setResults] = useState([]);
+  const [isFetching, setIsFetching] = useState(true)
+  const fetchAsync = async () => {
+    try {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/discover/movie?api_key=89cbd8f9a2829355783549b21d6e1769&language=en-US&sort_by=release_date.desc&include_adult=true&include_video=true&page=1&primary_release_date.gte=2010&release_date.gte=2010&vote_count.gte=6&vote_average.gte=6&with_watch_monetization_types=flatrate"
+      );
+      const data = await response.json();
+       setResults(getCollections(data.results));
+        setIsFetching(false)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchAsync();
+  }, []);
+  
 
   return (
+    isFetching ? <div>FETCHING DATA PLEASE WAIT</div> :
     <div className="overview">
       {results.map((result) => (
         <Card key="result.id" {...result} />
@@ -17,9 +36,12 @@ const Overview = () => {
 export default Overview;
 
 /******************************************************************************
- * 
+ *
  *              CREATE THE REACT REDUX STORE  TO DISPATCH THE DATA FROM FETCH
- * 
+ *
  *              WORK ON THE COLLECTION UTILS COMPONENT
+ *
+ *              CREATE A SPINNER COMPONENT
  * 
+
  * *************************************************************************** */
--- src/utils/collection.utils.js
+++ src/utils/collection.utils.js
@@ -1,38 +1,22 @@
-const getCollectionsAsync = async () => {
-  try {
-    const response = await fetch(
-      "https://api.themoviedb.org/3/discover/movie?api_key=89cbd8f9a2829355783549b21d6e1769&language=en-US&sort_by=release_date.desc&include_adult=true&include_video=true&page=1&primary_release_date.gte=2010&release_date.gte=2010&vote_count.gte=6&vote_average.gte=6&with_watch_monetization_types=flatrate"
-    );
-      const rawData = await response.json();
-      
-    return rawData.results.map(
-        ({
-          id,
-          overview,
-          popularity,
-          title,
-          release_date,
-          vote_average,
-          poster_path,
-        }) => ({
-          id,
-          overview,
-          popularity,
-          title,
-          release_date,
-          vote_average,
-          poster_path: `http://image.tmdb.org/t/p/w500/${poster_path}`,
-        })
-      );
-  } catch(error) {console.error(error);}
-};
+export const getCollections = (arr) =>
+  arr.map(
+    ({
+      id,
+      overview,
+      popularity,
+      title,
+      release_date,
+      vote_average,
+      poster_path,
+    }) => ({
+      id,
+      overview,
+      popularity,
+      title,
+      release_date,
+      vote_average,
+      poster_path: `http://image.tmdb.org/t/p/w500/${poster_path}`,
+    })
+  );
 
-const getCollections = () => {
-    fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=89cbd8f9a2829355783549b21d6e1769&language=en-US&sort_by=release_date.desc&include_adult=true&include_video=true&page=1&primary_release_date.gte=2010&release_date.gte=2010&vote_count.gte=6&vote_average.gte=6&with_watch_monetization_types=flatrate"
-    )
-        .then(res => res.json());
-}
-export default getCollectionsAsync;
-
-
+export default getCollections;
