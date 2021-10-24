34b7208f23541611371ffe7347ec1ed474ce4b65
Psalmseen <55616929+Psalmseen@users.noreply.github.com>
2021-10-24 16:55:39 +0100

    created  and included the spinner component

--- src/components/overview/overview.component.jsx
+++ src/components/overview/overview.component.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 // import results from "../../result";
 import Card from "../card/card.component";
-import  getCollections from "../../utils/collection.utils";
+import getCollections from "../../utils/collection.utils";
 import Spinner from "../spinner/spinner.component";
 
 const Overview = () => {
   const [results, setResults] = useState([]);
-  const [isFetching, setIsFetching] = useState(true)
+  const [isFetching, setIsFetching] = useState(true);
   const fetchAsync = async () => {
     try {
       const response = await fetch(
         "https://api.themoviedb.org/3/discover/movie?api_key=89cbd8f9a2829355783549b21d6e1769&language=en-US&sort_by=release_date.desc&include_adult=true&include_video=true&page=1&primary_release_date.gte=2010&release_date.gte=2010&vote_count.gte=6&vote_average.gte=6&with_watch_monetization_types=flatrate"
       );
       const data = await response.json();
-       setResults(getCollections(data.results));
-        setIsFetching(false)
+      setResults(getCollections(data.results));
+      setIsFetching(false);
     } catch (error) {
       console.error(error);
     }
@@ -22,10 +22,10 @@ const Overview = () => {
   useEffect(() => {
     fetchAsync();
   }, []);
-  
 
-  return (
-    isFetching ? <Spinner />:
+  return isFetching ? (
+    <Spinner />
+  ) : (
     <div className="overview">
       {results.map((result) => (
         <Card key="result.id" {...result} />
@@ -42,7 +42,7 @@ export default Overview;
  *
  *              WORK ON THE COLLECTION UTILS COMPONENT
  *
- *              CREATE A SPINNER COMPONENT
+ *              
  * 
 
  * *************************************************************************** */
