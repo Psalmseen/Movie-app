7596b2f3e01363e791f4953c10260e85bd88a7b8
Psalmseen <55616929+Psalmseen@users.noreply.github.com>
2021-11-11 00:20:24 +0100

    added hover to the card, worked on the footer and pagination, also worked on the margin of the sign in page, reduce the header size

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -2,37 +2,32 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer">
-    <span> contact us via: </span>
+    
     <nav>
       <a href="https://web.facebook.com/Sabes007" className="footer__link">
         <span>
           <i className="fab fa-facebook"></i>
         </span>
-        facebook
       </a>
       <a href="https://wa.link/0vilvz" className="footer__link">
         <span>
           <i className="fab fa-whatsapp"></i>
         </span>
-        whatsapp
       </a>
       <a href="https://github.com/psalmseen" className="footer__link">
         <span>
           <i className="fab fa-github"></i>
         </span>
-        github
       </a>
       <a href="mailto:samsonoyebamiji02@gmail.com" className="footer__link">
         <span>
           <i className="far fa-envelope"></i>
         </span>
-        mail
       </a>
       <a href="tel:+2348102545216" className="footer__link">
         <span>
           <i className="fas fa-phone"></i>
         </span>
-        phone
       </a>
     </nav>
   </div>
